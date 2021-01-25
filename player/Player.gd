86670842fe9d97cf881ec4c5969f83234c824f84
Julian <julianjo2@gmail.com>
2021-01-25 02:08:42 -0800

    added reset level button
    added a basic level reset button that just restarts the scene

--- player/Player.gd
+++ player/Player.gd
@@ -27,6 +27,8 @@ func _input(event):
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
 
 func _physics_process(delta):
 	var head_basis = head.get_global_transform().basis
