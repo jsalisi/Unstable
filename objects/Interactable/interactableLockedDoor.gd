814d55e8e0407ebfe4116e2574e2d94020d982c1
Julian <julianjo2@gmail.com>
2021-03-12 00:14:18 -0800

    Added locked door sound and notification

--- objects/Interactable/interactableLockedDoor.gd
+++ objects/Interactable/interactableLockedDoor.gd
@@ -1,18 +1,14 @@
 extends Interactable
 
 export (Resource) var locked_sound
-
-#onready var interaction_label = get_tree().get_root().find_node("InteractionLabel", true ,false)
+onready var action_label = get_tree().get_root().find_node("ActionLabel", true ,false)
 
 func get_interaction_text():
 	return "Open Door"
 	
 func interact():
-	var interaction_label = get_tree().get_root().find_node("InteractionLabel", true ,false)
-	interaction_label.set_text("Door stuck")
-	print("Door stuck")
-	interaction_label.set_visible(true)
-	
+	action_label.set_text("Door Stuck")
+	action_label.set_visible(true)
 	var audio_node = AudioStreamPlayer.new()
 	audio_node.stream = locked_sound
 	audio_node.volume_db = 2
@@ -21,3 +17,4 @@ func interact():
 	audio_node.play()
 	yield(get_tree().create_timer(1), "timeout")
 	audio_node.queue_free()
+	action_label.set_visible(false)
